c9094a5f0ef69a773847194326c79d3ba2d723bf
Fabravo2003 <fabian.bravo2201@alumnos.ubiobio.cl>
2025-11-13 22:46:06 -0300

    Version creo que final

--- tarea/src/main/java/IngS/tarea/repository/UsuarioRep.java
+++
@@ -1,7 +0,0 @@
-package IngS.tarea.repository;
-
-import IngS.tarea.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRep extends JpaRepository<Usuario, Long> {
-}
